27cff45a11412e110f93644cea8f42076644db73
laharithallapally <laharithallapally27@gmail.com>
2026-07-10 16:20:46 +0530

    Point frontend to Render backend instead of paused Railway backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://telangana-special-backend-production.up.railway.app/api",
+  baseURL: "https://telangana-special-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
